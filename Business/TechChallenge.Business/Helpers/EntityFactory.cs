b7b5e436e34dc55a62825eb8149c1f1ba5f005cb
Eddie Lonzanida <eddie.lonzanida@hotmail.com>
2017-10-18 17:41:45 +0800

    Added GetRaces Earger load

--- Business/TechChallenge.Business/Helpers/EntityFactory.cs
+++ Business/TechChallenge.Business/Helpers/EntityFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using Eml.Contracts.Repositories;
@@ -27,7 +28,7 @@ namespace TechChallenge.Business.Helpers
 
         public static async Task<List<Race>> GetRaces(IDataRepositorySoftDeleteInt<Race> repository)
         {
-            var response = await repository.GetAsync();
+            var response = await repository.GetAsync(r => r.Include(x => x.Horses));
             if (response == null) return await Task.FromResult(new List<Race>());
 
             return response
@@ -35,51 +36,5 @@ namespace TechChallenge.Business.Helpers
                 .ThenBy(r => r.Name)
                 .ToList();
         }
-
-
-        //public static async Task<List<Customer>> GetCustomers(string serviceUrl, IHttpService httpService)
-        //{
-        //    var url = $"{serviceUrl}/GetCustomers";
-        //    var aParams = new List<KeyValuePair<string, object>>
-        //    {
-        //        new KeyValuePair<string, object>("Name", "YourName")
-        //    };
-        //    var customers = await httpService.GetAsync<List<Customer>>(url, aParams);
-        //    var response = customers?
-        //        .OrderBy(r => r.Name)
-        //        .ThenBy(r => r.Id)
-        //        .ToList();
-        //    return response;
-        //}
-
-        //public static async Task<List<Bet>> GetBets(string serviceUrl, IHttpService httpService)
-        //{
-        //    var aParams = new List<KeyValuePair<string, object>>
-        //    {
-        //        new KeyValuePair<string, object>("Name", "YourName")
-        //    };
-        //    var url = $"{serviceUrl}/GetBetsV2";
-        //    var bets = await httpService.GetAsync<List<Bet>>(url, aParams);
-        //    return bets ?? new List<Bet>();
-        //}
-
-        //public static async Task<List<Race>> GetRaces(string serviceUrl, IHttpService httpService)
-        //{
-        //    var aParams = new List<KeyValuePair<string, object>>
-        //    {
-        //        new KeyValuePair<string, object>("Name", "YourName")
-        //    };
-        //    var url = $"{serviceUrl}/GetRaces";
-        //    var content = await httpService.GetAsync<List<Race>>(url, aParams);
-
-        //    if (content == null) return new List<Race>();
-        //    if (!content.Any()) return new List<Race>();
-
-        //    var response = content
-        //        .OrderBy(r => r.Start)
-        //        .ThenBy(r => r.Name)
-        //        .ToList();
-        //    return response;
-        //}
     }
 }
