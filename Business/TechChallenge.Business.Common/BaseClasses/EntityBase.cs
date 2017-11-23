7ce47d7dedcc84d8f59baa8529ba6e52b45b4778
Eddie Lonzanida <eddie.lonzanida@hotmail.com>
2017-11-23 20:12:27 +0800

    Updated NuGets

--- Business/TechChallenge.Business.Common/BaseClasses/EntityBase.cs
+++ Business/TechChallenge.Business.Common/BaseClasses/EntityBase.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Eml.Contracts.Entities;
 using Eml.SoftDelete;
+using Newtonsoft.Json;
 
 namespace TechChallenge.Business.Common.BaseClasses
 {
@@ -14,11 +15,13 @@ namespace TechChallenge.Business.Common.BaseClasses
         [Column(Order = 1)]
         public virtual int Id { get; set; }
 
+        [JsonIgnore]
         [Display(Name = "DateDeleted")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DateDeleted { get; set; }
 
+        [JsonIgnore]
         [Display(Name = "Reason for deleting:")]
         [DataType(DataType.MultilineText)]
         public string DeletionReason { get; set; }
